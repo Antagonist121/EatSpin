e070261ec5c28f198a32adca71f507894f2b1079
Antagonist121 <psyaww@nottingham.ac.uk>
2016-02-27 16:59:32 +0000

    Postcode search works correctly now

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 //document.getElementById("postcodeLauncher").onclick = doTheThing();
 
 
-function doTheThing(){
-	var app = angular.module('myApp', []);
-	app.controller('customersCtrl', function($scope, $http){
-			
-		$http.defaults.headers.common['Authorization'] = 'Basic VGVjaFRlc3RBUEk6dXNlcjI=';
-		$http.defaults.headers.common['Accept-Tenant'] = 'uk';
-		$http.defaults.headers.common['Accept-Version'] = '2';
+var app = angular.module('myApp', []).controller('customersCtrl', function($scope, $http){
 
-		$http.get("http://public.je-apis.com/restaurants?q=ng13gy").then(function(response) {
-			$scope.myData = [];
-			angular.forEach(response, function(value, key) {
-				angular.forEach(value.Restaurants, function(restaurant, key) {
-					this.push(restaurant);
-				}, $scope.myData);
-			});	
-		});
-	});
-	var myString = 
-		["<ul>",
-		"	<li ng-repeat=\"x in myData\">",
-		"		{{ x.Name }}",
-		"	</li>",
-		"</ul>"].join('\n');
+	var rests = this;
 
-	document.getElementById("theCode").innerHTML = myString;
-	alert("dun");
-}
+	rests.showValues = false;
+	rests.setRests = function(){
+
+		if(rests.yourPostcode == undefined){
+			alert("You must enter a postcode")
+		} else {
+			$http.defaults.headers.common['Authorization'] = 'Basic VGVjaFRlc3RBUEk6dXNlcjI=';
+			$http.defaults.headers.common['Accept-Tenant'] = 'uk';
+			$http.defaults.headers.common['Accept-Version'] = '2';
+
+			$http.get("http://public.je-apis.com/restaurants?q=" + rests.yourPostcode).then(function(response) {
+				$scope.myData = [];
+				angular.forEach(response, function(value, key) {
+					angular.forEach(value.Restaurants, function(restaurant, key) {
+						this.push(restaurant);
+					}, $scope.myData);
+				});	
+			});
+			rests.showValues = true;
+		}
+	};	
+});
